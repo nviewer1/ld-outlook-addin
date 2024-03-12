17184d150277fe10bdd1433e9e563a12b716c2e3
nxviewer <vxviewer@outlook.com>
2024-03-12 19:11:55 +0300

    start 2nd stage

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -14,15 +14,25 @@
     jQuery(document).ready(function(){
 
       function refresh() {
+        console.log('refresh() invoked');
+        let _userInfo = localStorage.getItem("userInfo");
         token= localStorage.getItem("token");
-        if(token){
+        if( token && _userInfo ) {
           console.log('if token - true');
-          let _userInfo = JSON.parse(localStorage.getItem("userInfo"));
+         _userInfo =JSON.parse(localStorage.getItem("userInfo"))
           $('#user-info-mail').text(_userInfo.email)
           setPage('u-loged');
+          Office.context.mailbox.item.notificationMessages.removeAsync("ActionPerformanceNotification");
         }else{
           console.log('if token - false');
           setPage('u-login');
+          const message = {
+            type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
+            message: "Нужно автозироваться для использования плагина",
+            icon: "Icon.80x80",
+            persistent: true,
+          };
+          Office.context.mailbox.item.notificationMessages.replaceAsync("ActionPerformanceNotification", message);
         }
       }
       refresh();
@@ -68,6 +78,79 @@
       });
 
 
+      $('#create-in-new-room-btn').on('click', function(e){
+        e.stopPropagation();
+        console.log('create-in-new-room-btn clicked');
+
+        Office.context.mailbox.item.subject.setAsync("<a href='https://whoer.net' target='_blank'>Some link</a>",
+        { coercionType: "html", },
+          function (asyncResult) {
+            if (asyncResult.status ==
+                Office.AsyncResultStatus.Failed) {
+                write(asyncResult.error.message);
+            }
+          }
+        );
+
+        // Office.context.mailbox.item.location.setAsync("https://whoer.net",
+        //   { coercionType: "html", },
+        //   function (asyncResult) {
+        //       if (asyncResult.status ==
+        //           Office.AsyncResultStatus.Failed) {
+        //           write(asyncResult.error.message);
+        //       }
+        //   }
+        // );
+        const locations = [
+          {
+            id: "Contoso",
+            type: Office.MailboxEnums.LocationType.Custom,
+            phone: '7988'
+          }
+
+        ];
+        Office.context.mailbox.item.enhancedLocation.addAsync(locations, (result) => {
+          if (result.status === Office.AsyncResultStatus.Succeeded) {
+            console.log(`Successfully added locations ${JSON.stringify(locations)}`);
+          } else {
+            console.error(`Failed to add locations. Error message: ${result.error.message}`);
+          }
+        });        
+        Office.context.mailbox.item.body.setAsync("<a href='https://whoer.net' target='_blank'> Мероприятие</a>",
+          { coercionType: "html", },
+          function (asyncResult) {
+              if (asyncResult.status ==
+                  Office.AsyncResultStatus.Failed) {
+                  write(asyncResult.error.message);
+              }
+          }
+        );
+
+
+        refresh();
+      });
+
+      $('#create-in-recent-room-btn').on('click', function(e){
+       
+
+      });      
+
+    //   function setLabelsForEvent() {
+    //     Office.context.mailbox.item.body.setAsync(
+    //         "Hello world!",
+    //         {
+    //             coercionType: "html", // Write text as HTML
+    //         },
+
+    //         // Callback method to check that setAsync succeeded
+    //         function (asyncResult) {
+    //             if (asyncResult.status ==
+    //                 Office.AsyncResultStatus.Failed) {
+    //                 write(asyncResult.error.message);
+    //             }
+    //         }
+    //     );
+    // }
 
       // When insert button is selected, build the content
       // and insert into the body.
